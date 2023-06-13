905ea4f793d6f08084182c28ee266f32d8023a9c
B Amulya Sai Rao <96655535+amulya-sai-rao@users.noreply.github.com>
2023-06-13 22:27:44 +0530

    usercard3

--- usercard3.js
+++ usercard3.js
@@ -5,16 +5,20 @@ let yuvrajoldinfo = {
 } 
 
 
-let displayobject;
+let displayobject = {
+    name : "",
+    imgurl : "",
+    description : ""
+};
 
 let getRandomUser = function(){
     fetch("https://randomuser.me/api")
     .then(response => response.json())
-    .then(data =>{
-        //displayobject.name = data.results[0].name.first + " " + data.results[0].name.last;
-        displayobject.name = Amulya;
-       // displauobject.imgurl = data.results[0].picture.large;
-        //displayobject.description = data.results[0].gender;
+    .then(data => {
+        displayobject.name = data.results[0].name.first + " " + data.results[0].name.last;
+        // displayobject.name = Amulya;
+       displauobject.imgurl = data.results[0].picture.large;
+        displayobject.description = data.results[0].gender;
         document.getElementById("ywc").src=displayobject.imgurl;
         document.getElementById("ywc-name").innerHTML=displayobject.name;
         document.getElementById("ywc-des").innerHTML = displayobject.description; 
