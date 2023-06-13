2babe2e7f295807b188416e5a2b8e03a405753d8
B Amulya Sai Rao <96655535+amulya-sai-rao@users.noreply.github.com>
2023-06-13 21:53:24 +0530

    usercard3

--- usercard3.js
+++ usercard3.js
@@ -4,24 +4,14 @@ let yuvrajoldinfo = {
     description: "Indian Cricketer"
 } 
 
-let yuvrajnewinfo = {
-    imgurl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTsHOTw0LvRC6fJhL4sgm1aQy-VfUdy0muM3zDfCcQpyg&usqp=CAU&ec=48665698",
-    name: "Yuvraj Singh",
-    description: "Player of the Series - World Cup 2011"
-}
 
-let isyuvrajold = true;
 let displayobject;
 
-let flipdata = function(){
-    if(isyuvrajold == true){
-        displayobject = yuvrajnewinfo;
-        isyuvrajold = false;
-    }
-    else{
-        displayobject = yuvrajoldinfo;
-        isyuvrajold = true;
-    }
+let getRandomUser = function(){
+    fetch("https://randomuser.me/api")
+    .then(response => response.json())
+    .then(data => console.log(data))
+
     document.getElementById("ywc").src=displayobject.imgurl;
     document.getElementById("ywc-name").innerHTML=displayobject.name;
     document.getElementById("ywc-des").innerHTML = displayobject.description;
