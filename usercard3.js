70755d4e06a973c63f0bfa8671e42a5d6cfb6d02
B Amulya Sai Rao <96655535+amulya-sai-rao@users.noreply.github.com>
2023-06-13 21:25:36 +0530

    Usercard3

---
+++ usercard3.js
@@ -0,0 +1,28 @@
+let yuvrajoldinfo = {
+    imgurl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTZY2wsI1h1dx-PBlDXI8YFDy_zezFsKeJvlB9VYF3EE7IYsJ9ukoi9JqqGznj7Fq_YxrTxufiwVqE&usqp=CAU&ec=48665698",
+    name: "Yuvraj Singh",
+    description: "Indian Cricketer"
+} 
+
+let yuvrajnewinfo = {
+    imgurl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTsHOTw0LvRC6fJhL4sgm1aQy-VfUdy0muM3zDfCcQpyg&usqp=CAU&ec=48665698",
+    name: "Yuvraj Singh",
+    description: "Player of the Series - World Cup 2011"
+}
+
+let isyuvrajold = true;
+let displayobject;
+
+let flipdata = function(){
+    if(isyuvrajold == true){
+        displayobject = yuvrajnewinfo;
+        isyuvrajold = false;
+    }
+    else{
+        displayobject = yuvrajoldinfo;
+        isyuvrajold = true;
+    }
+    document.getElementById("ywc").src=displayobject.imgurl;
+    document.getElementById("ywc-name").innerHTML=displayobject.name;
+    document.getElementById("ywc-des").innerHTML = displayobject.description;
+}
